7db651f28ab6123c5c965b7cba0db02abdce9503
abanob rafik <bobarafik0123@gmail.com>
2026-03-29 12:28:21 +0200

    complete dishes controller

--- controller/dishController.js
+++ controller/dishController.js
@@ -4,13 +4,11 @@ export const addNewDish = async (req, res) => {
   try {
     const { code } = req.body;
 
-    // تحقق إذا الطبق موجود بالفعل بالكود
     const exist = await dishSchema.findOne({ code });
     if (exist) {
       return res.status(400).json({ message: "Dish already exists" });
     }
 
-    // إنشاء الطبق الجديد وحفظه
     const newDish = new dishSchema(req.body);
     await newDish.save();
 
@@ -40,4 +38,50 @@ export const deleteDish = async (req, res) => {
   }
 };
 
-expo
+export const getAllDishes = async (req, res) => {
+  try {
+    const dishes = await dishSchema.find();
+    if (dishes.length === 0) {
+      return res.status(404).json({ message: "No dishes found" });
+    }
+    res.status(200).json({ message: "Dishes retrieved successfully", dishes });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving dishes", error: error.message });
+  }
+};
+
+export const getDishByCode = async (req, res) => {
+  try {
+    const { code } = req.body;
+    const dish = await dishSchema.findOne({ code });
+    if (!dish) {
+      return res.status(404).json({ message: "Dish not found" });
+    }
+    res.status(200).json({ message: "Dish retrieved successfully", dish });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving dish", error: error.message });
+  }
+};
+
+export const updateDish = async (req, res) => {
+  try {
+    const { code } = req.body;
+    const updatedDish = await dishSchema.findOneAndUpdate({ code }, req.body, {
+      new: true,
+    });
+    if (!updatedDish) {
+      return res.status(404).json({ message: "Dish not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "Dish updated successfully", dish: updatedDish });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error updating dish", error: error.message });
+  }
+};
--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,21 @@
 import express from "express";
-import { addNewDish, deleteDish } from "../controller/dishController.js";
+import {
+  addNewDish,
+  deleteDish,
+  getAllDishes,
+  getDishByCode,
+  updateDish,
+} from "../controller/dishController.js";
 
 const router = express.Router();
 
+//* for admin only
 router.post("/add", addNewDish);
 router.delete("/delete/:code", deleteDish);
+router.put("/update/:code", updateDish);
+
+// * for all users
+router.get("/getAll", getAllDishes);
+router.get("/getByCode/:code", getDishByCode);
 
 export default router;
